78d549c5f3a6d77f7ca75abf13159cc2634b2872
Irving De La Cruz Morales <148675404+IrvingDeLaCruzM@users.noreply.github.com>
2024-01-26 23:21:12 +0000

    Mensaje descriptivo de tus cambios

--- scripts.js
+++ scripts.js
@@ -1,9 +1,16 @@
 function validateLogin() {
     // Obtener los valores del formulario
+    var username = document.getElementById("username").value;
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
   
-    // Validar el correo electrónico usando una expresión regular simple
+    // Validar el nombre de usuario (puedes ajustar según tus requisitos)
+    if (username.trim() === "") {
+      alert("Por favor, ingrese un nombre de usuario.");
+      return;
+    }
+  
+    // Validar el correo electrónico usando una expresión regular
     var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
       alert("Por favor, ingrese un correo electrónico válido.");
